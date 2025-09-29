a4fe066c445b68e94534a151e63b891d14f7d6f7
hyosun <sunblisher@gmail.com>
2025-09-29 20:43:52 +0900

    design

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -5,10 +5,18 @@ function Header() {
     <header className="header">
       <nav className="c_inner">
         <ul className="menu">
-          <li className="list_item">Home</li>
-          <li className="list_item">Project</li>
-          <li className="list_item">Skill</li>
-          <li className="list_item">Contact</li>
+          <li className="list_item">
+            <span>Home</span>
+          </li>
+          <li className="list_item">
+            <span>Project</span>
+          </li>
+          <li className="list_item">
+            <span>Skill</span>
+          </li>
+          <li className="list_item">
+            <span>Contact</span>
+          </li>
         </ul>
       </nav>
     </header>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,52 @@
 import "../style/pages/home.css";
+import { useRef, useEffect } from "react";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
 
 function Home() {
+  const sectionRef = useRef(null);
+
+  useEffect(() => {
+    const targets = [
+      { el: ".title-left", x: -200 },
+      { el: ".title-right", x: 200 },
+    ];
+
+    // 시작 좌표 0으로 고정
+    gsap.set(
+      targets.map((t) => t.el),
+      { x: 0 }
+    );
+
+    // 패럴랙스 애니메이션
+    targets.forEach(({ el, x }) => {
+      gsap.to(el, {
+        x,
+        ease: "none", // 스크롤 = 애니메이션 1:1
+        scrollTrigger: {
+          trigger: sectionRef.current,
+          start: "top bottom",
+          end: "bottom top+=300", // ✅ 구간을 길게 잡음
+          scrub: 1,
+          invalidateOnRefresh: true, // 리사이즈 시 다시 계산
+        },
+      });
+    });
+  }, []);
+
   return (
     <div className="main">
-      <section className="hero">
+      <section className="hero" ref={sectionRef}>
         <div className="c_inner">
-          <h1>홈화면입니다.</h1>
+          <h1 className="title">
+            <span className="title-left">Hyosun</span>
+            <span className="title-right">Portfolio</span>
+          </h1>
         </div>
       </section>
+      <section className="test"></section>
     </div>
   );
 }
