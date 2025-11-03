2b482a22da03672ccdc8530f474e7c90f907d2bb
hyosun <sunblisher@gmail.com>
2025-11-03 18:22:47 +0900

    Fix GitHub Pages deployment: Add HashRouter and GitHub Actions workflow

--- src/app/router.jsx
+++ src/app/router.jsx
@@ -1,8 +1,8 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createHashRouter } from "react-router-dom";
 import DefaultLayout from "../layouts/DefaultLayout";
 import Home from "../pages/Home";
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: "/",
     element: <DefaultLayout />,
